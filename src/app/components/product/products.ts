ffe88c6eeb7d21322b585e6ba0727dca9988c534
Iryna Murzak <iramurzak@gmail.com>
2023-11-29 17:19:10 +0200

    update mock data

--- src/app/components/product/products.ts
+++ src/app/components/product/products.ts
@@ -4,23 +4,70 @@ import { Tags } from '../tag/tags';
 export const Products: IProduct[] = [
   {
     id: 1,
-    title: 'Product 1',
-    description: 'Description for product 1',
-    price: 10,
-    tags: [Tags[0], Tags[1]],
+    title: 'Portable Power Bank',
+    description:
+      'Never run out of battery on the go with this portable power bank.',
+    price: 15,
+    tags: [Tags[0]],
   },
   {
     id: 2,
-    title: 'Product 2',
-    description: 'Description for product 2',
-    price: 20,
-    tags: [Tags[2]],
+    title: 'Wireless Bluetooth Earbuds',
+    description:
+      'Experience crystal-clear sound with these wireless Bluetooth earbuds.',
+    price: 35,
+    tags: [Tags[0]],
   },
   {
     id: 3,
-    title: 'Product 3',
-    description: 'Description for product 3',
-    price: 15,
+    title: 'Elegant Coffee Mug',
+    description:
+      'Start your day with style using this elegant coffee mug. Perfect for home or office use.',
+    price: 5,
     tags: [Tags[3]],
+  },
+  {
+    id: 4,
+    title: 'Fitness Tracker',
+    description:
+      'Track your daily activities and stay fit with this advanced fitness tracker watch.',
+    price: 25,
+    tags: [Tags[0], Tags[2]],
+  },
+  {
+    id: 5,
+    title: 'Organic Cotton T-Shirt',
+    description:
+      'Stay comfortable and stylish in our eco-friendly organic cotton t-shirt.',
+    price: 5,
+    tags: [Tags[1]],
+  },
+  {
+    id: 6,
+    title: 'Cozy Home Blanket',
+    description: 'A soft and warm cotton blanket for your home.',
+    price: 20,
+    tags: [Tags[3]],
+  },
+  {
+    id: 7,
+    title: 'Luxury Beauty Kit',
+    description: 'Pamper yourself with this high-end beauty kit.',
+    price: 45,
+    tags: [Tags[4]],
+  },
+  {
+    id: 8,
+    title: 'Natural Beauty Skincare Set',
+    description: 'Organic skincare products for a radiant complexion.',
+    price: 15,
+    tags: [Tags[4]],
+  },
+  {
+    id: 9,
+    title: 'Aromatherapy Essential Oils Set',
+    description: 'Create a calming atmosphere with these essential oils.',
+    price: 35,
+    tags: [Tags[2], Tags[4]],
   },
 ];
--- src/app/components/tag/tags.ts
+++ src/app/components/tag/tags.ts
@@ -3,22 +3,27 @@ import { ITag } from '../../models/tag.model';
 export const Tags: ITag[] = [
   {
     id: 1,
-    name: 'Tag1',
+    name: 'Electronics',
     color: '#FCE700',
   },
   {
     id: 2,
-    name: 'Tag2',
+    name: 'Fashion',
     color: '#FF6D28',
   },
   {
     id: 3,
-    name: 'Tag3',
+    name: 'Wellness',
     color: '#F43B86',
   },
   {
     id: 4,
-    name: 'Tag4',
+    name: 'Home Decor',
     color: '#00C4FF',
+  },
+  {
+    id: 5,
+    name: 'Beauty',
+    color: '#00B676',
   },
 ];
